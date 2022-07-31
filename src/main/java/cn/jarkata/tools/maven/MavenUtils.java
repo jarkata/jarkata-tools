871b1c71542416ecae3782cf88013bc6799b7f2f
Jarkata <jarkata@foxmail.com>
2022-07-31 15:30:32 +0800

    增加maven 操作工具

--- src/main/java/cn/jarkata/tools/maven/MavenUtils.java
+++ src/main/java/cn/jarkata/tools/maven/MavenUtils.java
@@ -8,7 +8,18 @@ import java.util.Objects;
 
 public class MavenUtils {
 
-    public static void download(String groupId, String artifactId, String version) throws IOException,
+    /**
+     * 下载jar文件
+     *
+     * @param groupId
+     * @param artifactId
+     * @param version
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+
+    public static boolean download(String groupId, String artifactId, String version) throws IOException,
             InterruptedException {
         String mavenHome = System.getenv("MAVEN_HOME");
         Objects.requireNonNull(mavenHome, "MAVEN_HOME IS NULL");
@@ -20,12 +31,13 @@ public class MavenUtils {
         CmdResult cmdResult = CommandUtils.exec(split);
         String message = cmdResult.getMessage();
         int buildSuccess = message.indexOf("BUILD SUCCESS");
-        System.out.println(buildSuccess);
         if (buildSuccess > 0) {
             System.out.println("下载成功");
+            return true;
         } else {
             System.out.println("下载失败");
         }
-        System.out.println(cmdResult);
+        return false;
+
     }
 }
