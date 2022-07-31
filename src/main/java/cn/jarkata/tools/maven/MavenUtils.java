2125db37ebf9e31197a75571b3e1a7a4c8a4921e
Jarkata <jarkata@foxmail.com>
2022-07-31 12:19:00 +0800

    增加maven 操作工具

--- src/main/java/cn/jarkata/tools/maven/MavenUtils.java
+++ src/main/java/cn/jarkata/tools/maven/MavenUtils.java
@@ -4,12 +4,14 @@ import cn.jarkata.tools.command.CmdResult;
 import cn.jarkata.tools.command.CommandUtils;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MavenUtils {
 
     public static void download(String groupId, String artifactId, String version) throws IOException,
             InterruptedException {
         String mavenHome = System.getenv("MAVEN_HOME");
+        Objects.requireNonNull(mavenHome, "MAVEN_HOME IS NULL");
         String bin = mavenHome + "/bin/mvn";
         String cmd =
                 bin + " dependency:get -DgroupId=" + groupId + " -DartifactId=" + artifactId + " -Dversion=" + version;
