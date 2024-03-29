31aa02bb673f68c8e3f907c42ad009b297b74ee4
Jarkata <jarkata@foxmail.com>
2024-03-30 00:30:28 +0800

    feat:HttpUtils

--- src/main/java/cn/jarkata/tools/command/CommandUtils.java
+++ src/main/java/cn/jarkata/tools/command/CommandUtils.java
@@ -3,19 +3,28 @@ package cn.jarkata.tools.command;
 import cn.jarkata.commons.utils.FileUtils;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class CommandUtils {
 
-    public static CmdResult exec(String... cmd) throws IOException, InterruptedException {
-        ProcessBuilder builder = new ProcessBuilder(cmd);
-        Process process = builder.start();
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        FileUtils.copy(process.getInputStream(), outputStream);
-        FileUtils.copy(process.getErrorStream(), outputStream);
-        int value = process.waitFor();
-        return new CmdResult(value, outputStream.toString(StandardCharsets.UTF_8.name()));
+    public static CmdResult exec(String... cmd) throws Exception {
+        ByteArrayOutputStream outputStream = null;
+        ProcessBuilder builder;
+        try {
+            builder = new ProcessBuilder(cmd);
+            Process process = builder.start();
+            outputStream = new ByteArrayOutputStream();
+            FileUtils.copy(process.getInputStream(), outputStream);
+            FileUtils.copy(process.getErrorStream(), outputStream);
+            int value = process.waitFor();
+            return new CmdResult(value, outputStream.toString(StandardCharsets.UTF_8.name()));
+        } finally {
+            if (Objects.nonNull(outputStream)) {
+                outputStream.close();
+            }
+
+        }
     }
 
 }
